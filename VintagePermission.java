15d1281311df971f49946bcbb986b28454616d57
James Deaton <jamesdeaton72@gmail.com>
2018-04-05 16:48:35 -0500

    Create VintagePermission.java

---
+++ VintagePermission.java
@@ -0,0 +1,37 @@
+package me.VintageGaming.VintageVault;
+
+public class VintagePermission {
+
+  public String getName() {
+
+  }
+
+  public boolean addPlayerPermission(String player, String permission) {
+
+  }
+
+  public boolean removePlayerPermission(String player, String permission) {
+
+  }
+
+  public List<String> getPlayersPermissions(String player) {
+
+  }
+
+  public boolean setPlayerGroup(String player, String group) {
+
+  }
+
+  public boolean createGroup(String group) {
+
+  }
+
+  public boolean addGroupPermission(String group, String permission) {
+
+  }
+
+  public boolean removeGroupPermission(String group, String permission) {
+
+  }
+
+}
