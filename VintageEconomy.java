aeae07d025a73411f29ad9748dc819a3fb17adb7
James Deaton <jamesdeaton72@gmail.com>
2018-04-06 21:50:48 -0500

    Create VintageEconomy.java

---
+++ VintageEconomy.java
@@ -0,0 +1,20 @@
+package <yourPackage>;
+
+public class VintageEconomy {
+	
+	public static String getName() {
+		
+	}
+	
+	public static double getBalance(String player) {
+		
+	}
+	
+	public static double deposit(String player, double amount) {
+		
+	}
+	
+	public static double withdraw(String player, double amount) {
+		
+	}
+}
